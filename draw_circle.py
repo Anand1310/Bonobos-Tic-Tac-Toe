12621f6f10e32b49f991536aea8d1ec531b6278a
Olivia <Oliviavespera@gmail.com>
2021-07-08 04:00:59 +0800

    Fix draw_circle to print wider
    Terminal characters have narrower widths at 1:2 to height.
    It is necessary to fatten the draw_circle so that it prints a perfect circle.

--- draw_circle.py
+++ draw_circle.py
@@ -21,29 +21,39 @@ def draw_circle(center_x: int, center_y: int,
     x = 1
 
     print(term.on_color_rgb(*rgb))
-    print(f"{term.move_xy(center_x, center_y+radius) } ")
-    print(f"{term.move_xy(center_x, center_y-radius) } ")
-    print(f"{term.move_xy(center_x+radius, center_y) } ")
-    print(f"{term.move_xy(center_x-radius, center_y) } ")
+    print(f"{term.move_xy(center_x-1, center_y+radius) }   ")
+    print(f"{term.move_xy(center_x-1, center_y-radius) }   ")
+    print(f"{term.move_xy(center_x+radius*2, center_y) }  ")
+    print(f"{term.move_xy(center_x-radius*2-1, center_y) }  ")
+
     while x < y:
         y = int(sqrt(r2 - x ** 2) + 0.5)
-        draw_curve(center_x, center_y, x, y)
+        draw_curve(center_x, center_y, x*2, y)
+        draw_curve(center_x, center_y, x*2+1, y)
         x += 1
     while y > 0:
         x = int(sqrt(r2 - y ** 2) + 0.5)
-        draw_curve(center_x, center_y, x, y)
+        draw_curve(center_x, center_y, x*2, y)
+        draw_curve(center_x, center_y, x*2+1, y)
+
         y -= 1
-    
-def main(x: int, y: int, radius: int):
+
+def main():
     import atexit
 
     @atexit.register
     def Goodbye():
         print(term.normal+term.home+term.clear, end="")
 
+    x = 30
+    y = 15
+    radius = 14
+
     print(term.home + term.clear, end='')
     print(f"{term.on_grey} ", end="")
-    draw_circle(x, y, radius)
+
+    for i in range(1, radius, 2):
+        draw_circle(x, y, i)
 
     with term.cbreak():
         val=""
@@ -52,4 +62,4 @@ def main(x: int, y: int, radius: int):
             continue
 
 if __name__ == "__main__":
-    main(15, 15, 12)
+    main()
