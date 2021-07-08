9e2df4378e0f9de5c3e5de31520f194ca2530b2a
Olivia <Oliviavespera@gmail.com>
2021-07-08 22:26:11 +0800

    Modify parameters to draw_circle function and fix internal logic
    The draw circle was one character wider than it should be. This commit fixes it.
    x and y parameters are now combined into one tuple called coords.

--- draw_circle.py
+++ draw_circle.py
@@ -10,32 +10,30 @@ def draw_curve(center_x, center_y, x, y, fill):
     print(f"{term.move_xy(center_x-x, center_y+y)}{fill}")
     print(f"{term.move_xy(center_x-x, center_y-y)}{fill}")
 
-def draw_circle(center_x: int, center_y: int,
-                radius: int, rgb: Tuple=(0,255,0)):
+def draw_circle(coords: Tuple, radius: int,
+                rgb: Tuple=(0,255,0), fill: str="  "):
     """
     Draws a circle of a given radius at point center_x, center_y.
     Will be painted green if no RGB tuple is given.
     """
     r2 = radius ** 2
+    cx, cy = coords
     y = radius
     x = 1
 
     print(term.on_color_rgb(*rgb))
-    print(f"{term.move_xy(center_x-1, center_y+radius) }   ")
-    print(f"{term.move_xy(center_x-1, center_y-radius) }   ")
-    print(f"{term.move_xy(center_x+radius*2, center_y) }  ")
-    print(f"{term.move_xy(center_x-radius*2-1, center_y) }  ")
+    print(f"{term.move_xy(cx, cy+radius)}{fill}")
+    print(f"{term.move_xy(cx, cy-radius)}{fill}")
+    print(f"{term.move_xy(cx+radius*2, cy)}{fill}")
+    print(f"{term.move_xy(cx-radius*2, cy)}{fill}")
 
     while x < y:
         y = int(sqrt(r2 - x ** 2) + 0.5)
-        draw_curve(center_x, center_y, x*2, y)
-        draw_curve(center_x, center_y, x*2+1, y)
+        draw_curve(cx, cy, x*2, y, fill)
         x += 1
     while y > 0:
         x = int(sqrt(r2 - y ** 2) + 0.5)
-        draw_curve(center_x, center_y, x*2, y)
-        draw_curve(center_x, center_y, x*2+1, y)
-
+        draw_curve(cx, cy, x*2, y, fill)
         y -= 1
 
 def main():
@@ -45,15 +43,15 @@ def main():
     def Goodbye():
         print(term.normal+term.home+term.clear, end="")
 
-    x = 30
-    y = 15
-    radius = 14
+    coords = 30,8
+    radius = 8
 
     print(term.home + term.clear, end='')
     print(f"{term.on_grey} ", end="")
 
+    # draw_circle(coords, radius)
     for i in range(1, radius, 2):
-        draw_circle(x, y, i)
+        draw_circle(coords, i)
 
     with term.cbreak():
         val=""
